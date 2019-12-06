3d78b008fb2f9de17ca9baadcb151825dc645a44
jaewoo-L <lim945169@gmail.com>
2019-12-06 11:30:53 +0900

    section03 추가

--- js/partail.js
+++ js/partail.js
@@ -1,5 +1,7 @@
 var $firstMenu = $('nav ul.menu > li'),
 	$header = $('header'),
+	$firstMenuSelect = $('header .menu > li > a')
+	$menuSelect = $('header .menu ul li a'),
 	$top = $('#footer .btn-top');
 
 
@@ -20,3 +22,31 @@ var $firstMenu = $('nav ul.menu > li'),
             scrollTop: 0
         }, 500);
     });
+
+	$firstMenuSelect.mouseover(function() {
+		$(this).animate({
+			color: '#8c0c04'
+		},100)
+	})
+	.mouseout(function() {
+		$(this).animate({
+			color:'#303030'
+		},100)
+	})
+
+    $menuSelect.mouseover(function() {
+    	$(this).animate({
+    		color: '#8c0c04'
+    	},100);
+    	$(this).parents('ul').parents('li').children('a').css({
+    		color: '#8c0c04'
+    	})
+    })
+    .mouseout(function() {
+    	$(this).animate({
+    		color: '#8a8a8a'
+    	},100);
+    	$(this).parents('ul').parents('li').children('a').css({
+    		color: '#303030'
+    	})
+    })
--- js/slider.js
+++ js/slider.js
@@ -12,6 +12,12 @@ var newStoreContainer = document.querySelector('.new_store_slider_container'), /
 	newStoreautoSlide = undefined,
 	newStorePager = document.querySelector('.square-pager');
 
+var mallSliderContainer = document.querySelector('.tomnmall-container'), //ul
+	mallSlider = document.getElementsByClassName('tomnmall-slider'), //li
+	mallCurrentIdx = 0,
+	mallPagerHTML = '',
+	mallautoSlide = undefined,
+	mallPager = document.querySelector('.mall-pager');
 
 	
 	for(var i = 0; i < mainSlider.length; i++) {
@@ -19,39 +25,56 @@ var newStoreContainer = document.querySelector('.new_store_slider_container'), /
 		mainPagerHTML += "<span>" + i + "</span>"
 		mainPager.innerHTML = mainPagerHTML;
 	}
-	for(var i = 0; i < newStoreSlider.length; i++) {
-		newStoreSlider[i].style.left = i*100 + '%';
-		newStorePagerHTML += "<span>" + i + "</span>"
+	for(var j = 0; j < newStoreSlider.length; j++) {
+		newStoreSlider[j].style.left = j*100 + '%';
+		newStorePagerHTML += "<span>" + j + "</span>"
 		newStorePager.innerHTML = newStorePagerHTML;
+	}
+	for(var j = 0; j < mallSlider.length; j++) {
+		mallSlider[j].style.left = j*33.5 + '%';
+	}
+	for(var j = 0; j < mallSlider.length/3; j++) {
+		mallPagerHTML += "<span>" + j + "</span>"
+		mallPager.innerHTML = mallPagerHTML;
 	}
 
 
 var mainSliderBtn = document.querySelectorAll('.pager span'); //main btn
 var newStoreBtn = document.querySelectorAll('.square-pager span');
-
+var mallStoreBtn = document.querySelectorAll('.mall-pager span');
+console.log(mallStoreBtn);
 
 	function goToSlide(idx) {
 		mainSliderContainer.classList.add('animated');
-		mainCurrentIdx = idx;
 		for(var j = 0; j < mainSliderBtn.length; j++) {
 			mainSliderBtn[j].classList.remove('active');
 		}
 		mainSliderBtn[idx].classList.add('active');
 		mainSliderContainer.style.left = idx*(-100) + '%';
+		mainCurrentIdx = idx;		
 	}
 	function newStoreSlide(idx) {
 		newStoreContainer.classList.add('animated');
-		newStoreCurrentIdx = idx;
 		for(var j = 0; j < newStoreBtn.length; j++) {
 			newStoreBtn[j].classList.remove('active');
 		}
 		newStoreBtn[idx].classList.add('active');
 		newStoreContainer.style.left = idx*(-100) + '%';
-		
+		newStoreCurrentIdx = idx;
+	}
+	function mallStoreSlide(idx) {
+		mallSliderContainer.classList.add('animated');
+		for(var j = 0; j < mallStoreBtn.length; j++) {
+			mallStoreBtn[j].classList.remove('active');
+		}
+		mallStoreBtn[idx].classList.add('active');
+		mallSliderContainer.style.left = idx*(-100) + '%';
+		mallCurrentIdx = idx;
 	}
 
 	goToSlide(0);
 	newStoreSlide(0);
+	mallStoreSlide(0);
 
 	function startAutoSlide() {
 		autoSlide = setInterval(function() {
@@ -65,16 +88,27 @@ var newStoreBtn = document.querySelectorAll('.square-pager span');
 	}
 	function startAutoNewStoreSlide() {
 		newStoreautoSlide = setInterval(function() {	
-			var anextIdx = (newStoreCurrentIdx + 1) % newStoreSlider.length;
-			newStoreSlide(anextIdx);
+			var nextIdx = (newStoreCurrentIdx + 1) % newStoreBtn.length;
+			newStoreSlide(nextIdx);
 		},4000);
 	}
 	function stopAutoNewStoreSlide() {
 		clearInterval(newStoreautoSlide);
 	}
-
+	function startAutoMallSlide() {
+		mallautoSlide = setInterval(function() {
+			var nextIdx = (mallCurrentIdx + 1) % mallStoreBtn.length;
+			console.log(nextIdx);
+			mallStoreSlide(nextIdx);
+		},4000);
+	}
+	function stopAutoMallSlide() {
+		clearInterval(mallautoSlide);
+	}
 	startAutoSlide();
 	startAutoNewStoreSlide();
+	startAutoMallSlide();
+
 
 	mainSliderContainer.addEventListener('mouseenter', function() {
 		stopAutoSlide();
@@ -88,6 +122,12 @@ var newStoreBtn = document.querySelectorAll('.square-pager span');
 	newStoreContainer.addEventListener('mouseleave', function() {
 		startAutoNewStoreSlide();		
 	});
+	mallSliderContainer.addEventListener('mouseenter', function() {
+		stopAutoMallSlide();
+	});
+	mallSliderContainer.addEventListener('mouseleave', function() {
+		startAutoMallSlide();		
+	});
 
 	for(var i = 0 ; i < mainSliderBtn.length; i++) {
 		mainSliderBtn[i].addEventListener('click', function(event) {
@@ -99,6 +139,12 @@ var newStoreBtn = document.querySelectorAll('.square-pager span');
 		newStoreBtn[i].addEventListener('click', function(event) {
 			var pageNum1 = event.target.innerText;
 			newStoreSlide(pageNum1);
+		});
+	}
+	for(var i = 0 ; i < mallStoreBtn.length; i++) {
+		mallStoreBtn[i].addEventListener('click', function(event) {
+			var pageNum = event.target.innerText;
+			mallStoreSlide(pageNum);
 		});
 	}
 
